97f21c978fea2ed84f8d86a2edb9dca584c8e4bd
Lufermor <luisfergoza@gmai.com>
2022-03-03 18:41:44 +0100

    Continuando con el ejercicio 3

--- Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio3.java
+++ Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio3.java
@@ -2,6 +2,7 @@ package examenProgrTrimestre2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ExamenTrim2_Ejercicio3 {
@@ -11,16 +12,22 @@ public class ExamenTrim2_Ejercicio3 {
 	 * y 
 	 */
 	public static void informacionVideojuegos() {
+		ArrayList<String> consolas = new ArrayList<String>();
 		String fichero = "/Users/User/eclipse-workspace/Trimestre2/src/examenProgrTrimestre2/ventasVideojuegos.csv";
 		File file = new File(fichero);
+		int cuentaLineas=0;
 		try {
 			Scanner f = new Scanner(file);
 			f.nextLine();
 			while(f.hasNextLine()) {
 				String linea = f.nextLine();
-				String[] lineaSep = linea.split(";");
-				f.close(); //Se libera el fichero que estamos leyendo
+				cuentaLineas+=1;
+				String[] lineaSep = linea.split(",");
+				if(lineaSep.length > 11) {
+					
+				}
 			}
+			f.close(); //Se libera el fichero que estamos leyendo
 		} catch (FileNotFoundException e) {
 			System.out.println("El fichero " + fichero + " no ha podido ser leído.");
 		}
