bce20320996ededdf7d2f52abc97e1683f7a2931
Lufermor <luisfergoza@gmai.com>
2022-03-03 20:11:02 +0100

    Avanzando con el juego el rapido

--- Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio1.java
+++ Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio1.java
@@ -1,6 +1,7 @@
 package examenProgrTrimestre2;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ExamenTrim2_Ejercicio1 {
 	/*
@@ -9,8 +10,25 @@ public class ExamenTrim2_Ejercicio1 {
 	 */
 	public static void prepararPartida(ArrayList<Jugador> jugadores) {
 		for(int i =0; i<10; i++) {
-			jugadores.add(new Jugador("Jugador" + (i+1), (i+1)));
+			String nombre = "Jugador" + (i+1);
+			int dorsal = i+1;
+			jugadores.add(new Jugador(nombre, dorsal));
 		}
+	}
+	
+	/*
+	 * Pre: ---
+	 * Post: Este método desarrolla el juego, va eliminando jugadores 
+	 * e imprime por pantalla al último jugador que queda
+	 */
+	public static void jugar(ArrayList<Jugador> jugadores) {
+		while(jugadores.size()>1) {
+			for(int i = 0; i<jugadores.size(); i++) {
+				if(jugadores.size() == 1) break;
+				//if()
+			}
+		}
+		System.out.println("El jugador ganador es: \n" + jugadores.get(0).toString());
 	}
 	
 	/*
@@ -18,7 +36,14 @@ public class ExamenTrim2_Ejercicio1 {
 	 * Post: Este metodo main crea un arrayList de jugadores para jugar
 	 */
 	public static void main(String[] args) {
+		System.out.println("JUEGO \"EL RAPIDO\"\n"
+				+ "*presione enter para continuar*");
+		Scanner entrada = new Scanner(System.in);
+		entrada.nextLine();
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 		prepararPartida(jugadores);
+		jugar(jugadores);
+		System.out.println("Gracias por jugar :D");
+		entrada.close();
 	}
 }
