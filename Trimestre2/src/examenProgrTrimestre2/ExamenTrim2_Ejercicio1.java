2d1cddc41e7da413452a71b6c905978794725734
Lufermor <luisfergoza@gmai.com>
2022-03-03 19:43:33 +0100

    Clase Jugador creada

--- Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio1.java
+++ Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio1.java
@@ -1,5 +1,13 @@
 package examenProgrTrimestre2;
 
-public class ExamenTrim2_Ejercicio1 {
+import java.util.ArrayList;
 
+public class ExamenTrim2_Ejercicio1 {
+	/*
+	 * Pre: ---
+	 * Post: Este metodo main crea un arrayList de jugadores para jugar
+	 */
+	public static void main(String[] args) {
+		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+	}
 }
