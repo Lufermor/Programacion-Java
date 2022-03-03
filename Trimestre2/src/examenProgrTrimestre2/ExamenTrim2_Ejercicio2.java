e67c07fb50eb561555d163503d28a440011850ef
Lufermor <luisfergoza@gmai.com>
2022-03-03 17:21:03 +0100

    Inicios del ejercicio 2, examen trimestre 2

--- Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio2.java
+++ Trimestre2/src/examenProgrTrimestre2/ExamenTrim2_Ejercicio2.java
@@ -1,5 +1,22 @@
 package examenProgrTrimestre2;
 
-public class ExamenTrim2_Ejercicio2 {
+import java.util.Scanner;
 
+public class ExamenTrim2_Ejercicio2 {
+	/*
+	 * Pre: ---
+	 * Post: 
+	 */
+	public static int palabrasMasLargasQue(int lon, File file) {
+		
+	}
+	/*
+	 * Pre: ---
+	 * Post: Este método main
+	 */
+	public static void main(String[] args) {
+		String fichero = "/Users/User/eclipse-workspace/Trimestre2/src/examenProgrTrimestre2/textoExamen.txt";
+		Scanner entrada = new Scanner(System.in);
+		
+	}
 }
